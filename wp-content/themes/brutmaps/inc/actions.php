23d68c552c59ff5af5de2f32059d79c68aa2c620
andrujjjkeee <max.tkh.ua@gmial.com>
2019-11-18 11:00:33 +0200

    Brutmaps-wp upd admin-panel

--- wp-content/themes/brutmaps/inc/actions.php
+++ wp-content/themes/brutmaps/inc/actions.php
@@ -4,16 +4,33 @@ add_action( 'admin_menu', 'remove_menus' );
 
 function remove_menus(){
 
-    remove_menu_page( 'index.php' );                  
-    remove_menu_page( 'edit.php' );                   
-    remove_menu_page( 'upload.php' );                 
-    remove_menu_page( 'edit.php?post_type=page' );    
-    remove_menu_page( 'edit-comments.php' );          
-    remove_menu_page( 'themes.php' );                 
-    remove_menu_page( 'plugins.php' );                
-    remove_menu_page( 'users.php' );                 
-    remove_menu_page( 'tools.php' );                 
+    remove_menu_page( 'index.php' );
+    remove_menu_page( 'edit.php' );
+    remove_menu_page( 'upload.php' );
+    remove_menu_page( 'edit.php?post_type=page' );
+    remove_menu_page( 'edit-comments.php' );
+    remove_menu_page( 'themes.php' );
+    remove_menu_page( 'plugins.php' );
+//    remove_menu_page( 'users.php' );
+    remove_menu_page( 'tools.php' );
 
+}
+
+add_action( 'admin_init', 'remove_menu_nonadmin' );
+
+function remove_menu_nonadmin () {
+    global $user_ID;
+    if ( !current_user_can('administrator') ) {
+        remove_menu_page( 'index.php' );
+        remove_menu_page( 'edit.php' );
+        remove_menu_page( 'upload.php' );
+        remove_menu_page( 'edit.php?post_type=page' );
+        remove_menu_page( 'edit-comments.php' );
+        remove_menu_page( 'themes.php' );
+        remove_menu_page( 'plugins.php' );
+        remove_menu_page( 'users.php' );
+        remove_menu_page( 'tools.php' );
+    }
 }
 
 // Add Google Map Key
--- wp-content/themes/brutmaps/inc/cpt.php
+++ wp-content/themes/brutmaps/inc/cpt.php
@@ -35,6 +35,40 @@ function custom_post_type() {
     );
 
     register_post_type('sight', $args);
+
+    $labels = array(
+        'name' => 'Authors',
+        'singular_name' => 'Authors',
+        'menu_name' => 'Authors',
+        'all_items' => 'Authors',
+        'view_item' => 'Authors',
+        'add_new_item' => 'Add Author',
+        'add_new' => 'New Author',
+        'edit_item' => 'Edit',
+        'update_item' => 'Update',
+        'search_items' => 'Search'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'supports' => array('title'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_admin_bar' => true,
+        'can_export' => true,
+        'has_archive' => false,
+        'exclude_from_search' => true,
+        'publicly_queryable' => true,
+        'capability_type' => 'post',
+        'show_in_rest' => true,
+        'rewrite' => array(
+            'with_front' => true
+        )
+    );
+
+    register_post_type('authors', $args);
 }
 
 add_action('init', 'custom_post_type');
