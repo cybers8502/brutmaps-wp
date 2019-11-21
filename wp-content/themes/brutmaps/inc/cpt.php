e26a401752f3060666303f979372310f03678bc8
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-11-21 17:24:09 +0200

    [BRUT-25] added file uploader

--- wp-content/themes/brutmaps/inc/cpt.php
+++ wp-content/themes/brutmaps/inc/cpt.php
@@ -36,6 +36,40 @@ function custom_post_type() {
 
     register_post_type('sight', $args);
 
+	$labels1 = array(
+		'name' => 'Architect',
+		'singular_name' => 'Architect',
+		'menu_name' => 'Architect',
+		'all_items' => 'Architect',
+		'view_item' => 'Architect',
+		'add_new_item' => 'Add Showplace',
+		'add_new' => 'New Showplace',
+		'edit_item' => 'Edit',
+		'update_item' => 'Update',
+		'search_items' => 'Search'
+	);
+
+	$args1 = array(
+		'labels' => $labels1,
+		'supports' => array('title'),
+		'hierarchical' => false,
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'show_in_admin_bar' => true,
+		'can_export' => true,
+		'has_archive' => false,
+		'exclude_from_search' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+		'show_in_rest' => true,
+		'rewrite' => array(
+			'with_front' => true
+		)
+	);
+
+	register_post_type('architect', $args1);
+
     $labels = array(
         'name' => 'Authors',
         'singular_name' => 'Authors',
