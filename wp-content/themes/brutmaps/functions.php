38ccbbafee5c0e48c8c862feed849b9e7d66ca93
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-11-22 16:17:56 +0200

    [BRUT-24] added options page

--- wp-content/themes/brutmaps/functions.php
+++ wp-content/themes/brutmaps/functions.php
@@ -3,10 +3,6 @@ define( 'TEMPLATEINC', TEMPLATEPATH . '/inc' );
 define( 'ABOUT', 1211);
 show_admin_bar( false );
 
-@ini_set( 'upload_max_size' , '64M' );
-@ini_set( 'post_max_size', '64M');
-@ini_set( 'max_execution_time', '300' );
-
 require_once( TEMPLATEINC . '/cpt.php' );
 require_once( TEMPLATEINC . '/actions.php' );
 require_once( TEMPLATEINC . '/api.php' );
