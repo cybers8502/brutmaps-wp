7c4e2786154fcb84af0c1e46520cbfb73bc697c5
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-11-22 09:41:08 +0200

    [BRUT-24] added options page

--- wp-content/themes/brutmaps/inc/api.php
+++ wp-content/themes/brutmaps/inc/api.php
@@ -264,11 +264,13 @@ function getSights() {
 
 function notifyAboutNewSight() {
 	$emails = get_field('notifiedUsers', 'options');
+	var_dump($emails);
 	if (is_array($emails) && count($emails) > 0) {
 		$emailsArray = [];
 		foreach ($emails as $item) {
 			$emailsArray[] = $item['email'];
 		}
+		var_dump($emailsArray);
 		$to = $emailsArray;
 		$subject = 'New sight offer on BRUTMAPS';
 		$body = 'We have a new offered sight';
