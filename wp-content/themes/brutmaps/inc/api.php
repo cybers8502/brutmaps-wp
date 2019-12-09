49aa7c1d24e5d8593898d5c0548a0eea1b28d385
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-12-09 15:23:30 +0200

    Changed encoding of body data

--- wp-content/themes/brutmaps/inc/api.php
+++ wp-content/themes/brutmaps/inc/api.php
@@ -44,7 +44,7 @@ function API_GET_ABOUT_DATA() {
 		}
 		$title = get_the_title(ABOUT);
 		$mainData = [
-			'title'             => wp_json_encode($title),
+			'title'             => json_encode($title),
 			'main_image'        => $mainImage,
 			'description_1'     => get_field('description_1', ABOUT),
 			'gallery_sub_text'  => get_field('gallery_sub_text', ABOUT),
