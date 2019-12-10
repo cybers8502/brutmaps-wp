07c76dd1190596c7cb007261adc108aa766f3a5a
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-12-10 13:47:31 +0200

    added image width/height calculation

--- wp-content/themes/brutmaps/single-sight.php
+++ wp-content/themes/brutmaps/single-sight.php
@@ -5,15 +5,11 @@
 
 </style>
 <?php
-$gallery = get_field('gallery', ABOUT);
-foreach ($gallery as $item) {
-	var_dump(wp_get_attachment_image_src( $item['gallery_image']['ID'], 'full' ));
-}
-//var_dump( get_field('capital_and_state') );
-//var_dump( get_field('established') );
-//var_dump( get_field('main_image') );
-//var_dump( get_field('main_content') );
-//var_dump( get_field('gallery') );
-//var_dump( get_field('extra_date') );
-//var_dump( get_field('location') );
+var_dump( get_field('capital_and_state') );
+var_dump( get_field('established') );
+var_dump( get_field('main_image') );
+var_dump( get_field('main_content') );
+var_dump( get_field('gallery') );
+var_dump( get_field('extra_date') );
+var_dump( get_field('location') );
 ?>
