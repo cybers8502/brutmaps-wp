45c2f1c1699fd1c122323c7e57fa010ad7bb434a
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-12-11 17:48:20 +0200

    fake images added

--- wp-content/themes/brutmaps/inc/actions.php
+++ wp-content/themes/brutmaps/inc/actions.php
@@ -5,12 +5,9 @@ add_action( 'admin_menu', 'remove_menus' );
 function remove_menus(){
     remove_menu_page( 'index.php' );
     remove_menu_page( 'edit.php' );
-//    remove_menu_page( 'upload.php' );
-//    remove_menu_page( 'edit.php?post_type=page' );
     remove_menu_page( 'edit-comments.php' );
     remove_menu_page( 'themes.php' );
     remove_menu_page( 'plugins.php' );
-//    remove_menu_page( 'users.php' );
     remove_menu_page( 'tools.php' );
 }
 
@@ -21,8 +18,6 @@ function remove_menu_nonadmin () {
     if ( !current_user_can('administrator') ) {
         remove_menu_page( 'index.php' );
         remove_menu_page( 'edit.php' );
-//        remove_menu_page( 'upload.php' );
-        remove_menu_page( 'edit.php?post_type=page' );
         remove_menu_page( 'edit-comments.php' );
         remove_menu_page( 'themes.php' );
         remove_menu_page( 'plugins.php' );
