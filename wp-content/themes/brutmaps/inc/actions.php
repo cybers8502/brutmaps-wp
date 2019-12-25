1b693bd343f636cf7bf3cefb251b43edfcabe2d4
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-12-25 14:51:43 +0200

    plugins is visible now

--- wp-content/themes/brutmaps/inc/actions.php
+++ wp-content/themes/brutmaps/inc/actions.php
@@ -7,7 +7,7 @@ function remove_menus(){
     remove_menu_page( 'edit.php' );
     remove_menu_page( 'edit-comments.php' );
     remove_menu_page( 'themes.php' );
-    remove_menu_page( 'plugins.php' );
+//    remove_menu_page( 'plugins.php' );
     remove_menu_page( 'tools.php' );
 }
 
