23f03f182b0d0463fc78fc38580a37a2167a56f3
Alexander Nikolaychuk <alexksnikol@gmail.com>
2019-12-25 15:55:49 +0200

    tools is visible now

--- wp-content/themes/brutmaps/inc/actions.php
+++ wp-content/themes/brutmaps/inc/actions.php
@@ -7,8 +7,6 @@ function remove_menus(){
     remove_menu_page( 'edit.php' );
     remove_menu_page( 'edit-comments.php' );
     remove_menu_page( 'themes.php' );
-//    remove_menu_page( 'plugins.php' );
-    remove_menu_page( 'tools.php' );
 }
 
 add_action( 'admin_init', 'remove_menu_nonadmin' );
