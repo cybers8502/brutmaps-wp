8bb38f91028e7550d162cda2c9c985aba8ef03a2
Alexander Nikolaychuk <alexksnikol@gmail.com>
2020-10-27 10:51:53 +0200

    [update] added author fields

---
+++ wp-content/themes/brutmaps/inc/authors/author_acf_fields.php
@@ -0,0 +1,80 @@
+<?php
+
+if( function_exists('acf_add_local_field_group') ):
+    acf_add_local_field_group(array (
+        'key' => 'authors',
+        'title' => 'Author',
+        'fields' => get_author_acf_fields(),
+        'location' => array (
+            array (
+                array (
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'authors',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+    ));
+    add_action('acf/init', 'acf_add_local_field_group');
+endif;
+
+function get_author_acf_fields() {
+    return array(
+        get_author_first_name_field(),
+        get_author_last_name_field(),
+        get_author_link_field(),
+        get_author_instagram_field()
+    );
+}
+
+function get_author_first_name_field() {
+    return array (
+        'key' => 'first_name_author',
+        'label' => 'First name',
+        'name' => 'first_name_author',
+        'type' => 'text',
+        'required' => 0,
+        'wrapper' => array (
+            'width' => '50',
+        )
+    );
+}
+
+function get_author_last_name_field() {
+    return array (
+        'key' => 'second_name_author',
+        'label' => 'Last name',
+        'name' => 'second_name_author',
+        'type' => 'text',
+        'required' => 0,
+        'wrapper' => array (
+            'width' => '50',
+        )
+    );
+}
+
+function get_author_instagram_field() {
+    return array (
+        'key' => 'instagram1',
+        'label' => 'Instagram',
+        'name' => 'instagram',
+        'type' => 'text',
+        'required' => 1
+    );
+}
+
+function get_author_link_field() {
+    return array (
+        'key' => 'link',
+        'label' => 'Link',
+        'name' => 'link',
+        'type' => 'text',
+        'required' => 0
+    );
+}
--- wp-content/themes/brutmaps/inc/contributor/contributor_acf_fields.php
+++ wp-content/themes/brutmaps/inc/contributor/contributor_acf_fields.php
@@ -2,15 +2,15 @@
 
 if( function_exists('acf_add_local_field_group') ):
     acf_add_local_field_group(array (
-        'key' => 'associated',
-        'title' => 'Associated',
-        'fields' => get_associated_acf_fields(),
+        'key' => 'contributor',
+        'title' => 'Contributor',
+        'fields' => get_contributor_acf_fields(),
         'location' => array (
             array (
                 array (
                     'param' => 'post_type',
                     'operator' => '==',
-                    'value' => 'associated_people',
+                    'value' => 'contributor',
                 ),
             ),
         ),
@@ -24,16 +24,17 @@ if( function_exists('acf_add_local_field_group') ):
     add_action('acf/init', 'acf_add_local_field_group');
 endif;
 
-function get_associated_acf_fields() {
+function get_contributor_acf_fields() {
     return array(
-        get_associated_first_name_field(),
-        get_associated_last_name_field(),
-        get_associated_link_field(),
-        get_associated_image_field()
+        get_contributor_first_name_field(),
+        get_contributor_last_name_field(),
+        get_contributor_email_field(),
+        get_contributor_link_field(),
+        get_contributor_linked_objects_field()
     );
 }
 
-function get_associated_first_name_field() {
+function get_contributor_first_name_field() {
     return array (
         'key' => 'first_name',
         'label' => 'First name',
@@ -46,7 +47,7 @@ function get_associated_first_name_field() {
     );
 }
 
-function get_associated_last_name_field() {
+function get_contributor_last_name_field() {
     return array (
         'key' => 'last_name',
         'label' => 'Last name',
@@ -59,7 +60,20 @@ function get_associated_last_name_field() {
     );
 }
 
-function get_associated_link_field() {
+function get_contributor_email_field() {
+    return array (
+        'key' => 'email',
+        'label' => 'E-mail',
+        'name' => 'email',
+        'type' => 'text',
+        'required' => 1,
+        'wrapper' => array (
+            'width' => '50',
+        )
+    );
+}
+
+function get_contributor_link_field() {
     return array (
         'key' => 'link',
         'label' => 'Link',
@@ -72,16 +86,15 @@ function get_associated_link_field() {
     );
 }
 
-function get_associated_image_field() {
+function get_contributor_linked_objects_field() {
     return array (
-        'key' => 'image',
-        'label' => 'Avatar',
-        'name' => 'image',
-        'type' => 'image',
+        'key' => 'linked_sights',
+        'label' => 'linked objects',
+        'name' => 'linked_sights',
+        'type' => 'post_object',
         'required' => 0,
-        'return_format' => 'array',
-        'wrapper' => array (
-            'width' => '50',
-        )
+        'post_type' => 'contributor',
+        'return_format' => 'id',
+        'multiple' => 1
     );
 }
