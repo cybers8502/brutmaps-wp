03c28aed50a29299bec494aa485276411b6e52bd
Alexander Nikolaychuk <alexksnikol@gmail.com>
2020-10-28 00:03:56 +0200

    [update] dublicates fields fixed

--- wp-content/themes/brutmaps/header.php
+++ wp-content/themes/brutmaps/header.php
@@ -124,6 +124,7 @@ var_dump(count($posts_array));
 
 foreach ($posts_array as $post_val) {
     echo $post_val->ID;
-    update_field( 'main_image', true , $post_val->ID);
+    echo get_field('options_main_image', $post_val->ID));
+//    update_field( 'main_image', true , $post_val->ID);
 }
 ?>
